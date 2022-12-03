6c2ece1502eb49922043684018540199c4613dbd
Marialetto <113329575+Marialetto@users.noreply.github.com>
2022-12-03 21:44:51 +0300

    Update console.cpp

--- console.cpp
+++ console.cpp
@@ -5,36 +5,67 @@
 #include "console.h"
 using namespace std;
 console::console(int argc,char **argv){
-int in_string;
-while((in_string = getopt(argc, argv, "i:p:o:r:h")) != -1) {  
-    switch(in_string) { 
-    case 'i': // ip- adress
-        help(argv[0]);
-        break;
-    case 'p': // port adress
-        port="33333";
-        help(argv[0]);
-        break;
-    case 'a': // name auth file
-        auth="vclient.vonf";
-        help(argv[0]);
-        break;
-    case 'o': // name original file
-        help(argv[0]);
-        break;
-    case 'r': // name result file
-        help(argv[0]);
-        break;
-    case 'h': //  help 
-        help(argv[0]);
-        break;
-    default:
-        cout <<"-h:" << endl;
-        help(argv[0]);
-        }
-    } 
+    int opt;
+    while((opt = getopt(argc, argv, "i:p:o:r:h")) != -1) {
+        switch(opt) {
+        case 'i': // ip- adress
+            ipLen=strtol(optarg,nullptr,10);
+            if(ipLen>9 ) {
+                ip = optarg;
+            } else {
+                std::cerr<<"ipLen must be more 9\n";
+                help(argv[0]);
+            }
+            break;
+        case 'p': // port adress
+            portLen = char(strtol(optarg,nullptr,10));
+            if (portLen=0 ) {
+                port=33333;
+            } else if (portLen=5) {
+                port=char(*optarg);
+            } else {
+                std::cerr<<"portLen must be equal to 5\n";
+                help(argv[0]);
+            }
+            break;
+        case 'a': // name auth file
+            NameLen = strtol(optarg,nullptr,10);
+            if (NameLen=0) {
+                auth="~/.config/vclient.conf";
+            } else if (NameLen <= 6) {
+               auth=optarg;
+            } else {
+                std::cerr<<"NameLen must be more 6";
+                help(argv[0]);
+            }
+            break;
+        case 'o': // name original file
+            NameLen = strtol(optarg,nullptr,10);
+            if (NameLen <=6) {
+               original=optarg;
+            } else {
+                std::cerr<<"NameLen must be more 6\n";
+                help(argv[0]);
+            }
+            break;
+        case 'r': // name result file
+            NameLen = strtol(optarg,nullptr,10);
+            if(NameLen<=6 ) {
+               Result = optarg;
+            } else{
+                std::cerr<<"NameLen must be more 6\n";
+                help(argv[0]);
+            }
+            break;
+        case 'h': // help
+        case '?': // неверный параметр
+        case ':': // нет значения у параметра
+            help(argv[0]);
+        }        
+    }
 }
+
 void console::help(const char* progName){
-    cout<<"Usage: "<< progName << " [-i ip adress servera][-p port servera][-a name auto file][-o name original file][-r name result file]/n";
+    cout<<"Usage: "<< progName<< "\n -i server address, \n -o name original file, \n -r name result file,\n [-p port servera],\n [-a name auth file]\n";
     exit(1);
 }
