c21554b1d8b02a75e18d1ccbdefbb2aa0737edec
Marialetto <113329575+Marialetto@users.noreply.github.com>
2022-12-17 10:53:56 +0300

    Update operator.cpp

--- operator.cpp
+++ operator.cpp
@@ -1,35 +1,29 @@
 #include "operator.h"
 using namespace std;
-void Operator::read(){
-    //uint16_t n;
-    //vector<uint16_t> in;
-    ifstream file(original);
-	if (!file) {
-        std::cerr<<"No open original file\n";
-        exit(1);
+const char *original="original.txt";
+void Operator::read(const char *original, vector<uint16_t>in, uint16_t line){
+ifstream file(original);
+if (!file) {
+	cerr<<"No open original file\n";
+	exit(1);
 }
-    while (!file.eof())
-    {
-        file >> n;
-        in.push_back(n);
-    }
-    file.close();
-	}
+while (!file.eof()){
+	file >> line;
+	in.push_back(line);
+}
+	file.close();
+}
 
-
-void Operator::write() {
-	//uint16_t n;
- 	//vector<uint16_t> out;
-    ifstream file(Result);
-    if (!file) {
-        std::cerr<<"No open result file\n";
-        exit(1);
-    } else {
-         cout <<"File is open for write\n";
-         while (!file.eof()) {
-         		file << n;
-               	out.push_back(n);
-         }
-        file.close();
-        }
-    }
+const char *Result="Result.txt";
+void Operator::write(const char *Result, vector<uint16_t>out, uint16_t line){
+ofstream file(Result);
+if (!file) {
+    cerr<<"No open result file\n";
+    exit(1);
+}
+while (!file.eof()) {
+       file << line;
+       out.push_back(line);
+}
+       file.close();
+}
