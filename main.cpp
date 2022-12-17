f62a234da5dd77a59d3bb73ec3beadc6c56d9898
Marialetto <113329575+Marialetto@users.noreply.github.com>
2022-12-17 10:52:07 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,30 @@
-#include <iostream>
-#include <cstdlib>
-#include <unistd.h>
-#include <string>
 #include "console.h"
-//#include "conect.h"
-//#include "operator.h"
+#include "connect.h"
+#include "operator.h"
 using namespace std;
 int main(int argc, char **argv){
+//concole.h
     console op(argc,argv);
     cout<<op.getIP()<<endl;
     cout<<op.getPort()<<endl;
     cout<<op.getAuth()<<endl;
     cout<<op.getOriginal()<<endl;
     cout<<op.getResult()<<endl;
+//operator.h
+
+//connect.h
+    connect c;
+
+    //connect to host
+    c.conn(host , port);
+     
+    //send some data
+    c.send_data(".....");
+     
+    //receive and echo reply
+    cout<<"----------------------------\n\n";
+    cout<<c.receive(1024);
+    cout<<"\n\n----------------------------\n\n";
+   
     return 0;
 }
