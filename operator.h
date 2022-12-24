fb73d9d73e0125097c13578272ce449f6cd37c61
Marialetto <113329575+Marialetto@users.noreply.github.com>
2022-12-24 11:52:20 +0300

    Update operator.h

--- operator.h
+++ operator.h
@@ -5,14 +5,14 @@
 using namespace std;
 class Operator{
 private:
-const char *original;
-const char *Result;
+	const char *original;
+	const char *Result;
 	uint16_t line;
-    vector<uint16_t>in;
-    vector<uint16_t>out;
+   	vector<uint16_t>in;
+    	vector<uint16_t>out;
 public:
-    Operator()=delete;
+    	Operator()=delete;
 	Operator(const char *original,const char *Result);
 	void read(const char *original, vector<uint16_t>in, uint16_t line);
-    void write(const char *Result, vector<uint16_t>out, uint16_t line);
+   	void write(const char *Result, vector<uint16_t>out, uint16_t line);
 };
