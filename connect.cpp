c36f501eaf7c1ca49e257e7ac4effb7efb721572
Marialetto <113329575+Marialetto@users.noreply.github.com>
2022-12-24 11:34:09 +0300

    Update connect.cpp

--- connect.cpp
+++ connect.cpp
@@ -1,81 +1,143 @@
+
 #include "connect.h"
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 using namespace std;
+#define bufSize 512 
 
-//Connect to a host on a certain port number
-bool connect::conn(string address , unsigned short int port)
+#include <cryptopp/cryptlib.h>
+#include <cryptopp/hex.h>
+#include <cryptopp/files.h>
+#include <cryptopp/md5.h>
+using namespace CryptoPP;
+
+#define CRYPTOPP_ENABLE_NAMESPACE_WEAK 1
+   
+void connect::connect(int Socket,unsigned short int port, string ip)
 {
-    if(sock == -1)
-    {
-        sock = socket(AF_INET , SOCK_STREAM , 0);
-        if (sock == -1)
-        {
-            perror("Could not create socket");
-        }
-         
-        cout<<"Socket created\n";
-    }  
-    if(inet_addr(address.c_str()) == -1)
-    {
-        struct hostent *he;
-        struct in_addr **addr_list;
-         
-        if ( (he = gethostbyname( address.c_str() ) ) == NULL)
-        {
-            herror("gethostbyname");
-            cout<<"Failed to resolve hostname\n";
-            return false;
-        }
-         
-        addr_list = (struct in_addr **) he->h_addr_list;
+    //Адрес программы-клиента
+    sockaddr_in *cAddr = new (sockaddr_in);
+    cAddr->sin_family = AF_INET; 
+   	cAddr->sin_port = 0; 
+    cAddr->sin_addr.s_addr = 0;
+    
+    //Адрес программы-сервера
+    sockaddr_in * sAddr = new (sockaddr_in);
+    sAddr->sin_family = AF_INET; 
+    sAddr->sin_port = htons(port); 
+    sAddr->sin_addr.s_addr = inet_addr(ip); // все адреса нашего пк
+    
+    //буфер для передачи и приема данных
+  	char *buf = new char[bufSize];
+    
+    // создание сокета
+    Socket = socket(AF_INET, SOCK_STREAM, 0);//TCP 
+    if (Socket == -1) {
+    	cerr << "Error open socket\n";
+        close(Socket);
+        exit(1);
+    }
+    int rc = bind(Socket, (const sockaddr *) cAddr, sizeof (sockaddr_in));
+    if (rc == -1) {
+    	cerr <<"Error bind socket with local address\n";
+        close(Socket);
+        exit(1);
+    }
+    //установка соединения
+    rc = connect(Socket, (const sockaddr*) sAddr, sizeof(sockaddr_in));
+    if (rc == -1) {
+        cerr <<"Error connect socket with server\n";
+        close(Socket);
+        exit(1);
+    }
+ }`
  
-        for(int i = 0; addr_list[i] != NULL; i++)
-        {
-            server.sin_addr = *addr_list[i];
-             
-            cout<<address<<" resolved to "<<inet_ntoa(*addr_list[i])<<endl;
-             
-            break;
-        }
-    }
-     
-    else
-    {
-        server.sin_addr.s_addr = inet_addr( address.c_str() );
-    }
-    server.sin_family = AF_INET;
-    server.sin_port = htons( port );
-    if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0)
-    {
-        perror("connect failed. Error");
-        return 1;
-    }
-    cout<<"Connected\n";
-    return true;
-}
-    //Отправить данные на подключенный хост
-bool connect::send_data(vector<uint16_t>in)
+void connect::Auth(string auth)
 {
-    if( send(sock , data.c_str() , strlen( data.c_str() ) , 0) < 0)
-    {
-        perror("Send failed : ");
-        return false;
+char buf [bufSize];
+int msgLen = auth.length(); // вычисляем длину строки
+size_t length = auth.copy(buf,msgLen); // копируем строку
+rc=send(Socket, buf, sizeof(buf), 0);// Отправляем файл для аутентификации
+delete[] buf;
+int msgLen = username.length(); // вычисляем длину строки
+size_t length = username.copy(buf,msgLen); // копируем строку
+rc=send(Socket, buf, sizeof(buf), 0);// Отправляем файл для аутентификации
+delete[] buf;
+bytes_read = recv(sock, buf, sizeof(buf), 0); //Принимаем строку с SALT или ошибку ERR
+buf[bytes_read] = '\0';
+string s1 = string(buf);
+ if (s1 == "ERR")
+ {
+    cerr << "Identification error" << endl;
+    close(Socket);
+    exit(1);
+ }
+ else 
+ {
+ string s2 = s1 + pswd;
+ int msgLen = s2.length(); // вычисляем длину строки
+ size_t length = s2.copy(buf,msgLen); // копируем строку
+ string hash,Hash;
+ Weak::MD5 hash;
+StringSource(hash, true, new HashFilter(hash, new HexEncoder(new StringSink (Hash))));
+
+ int msgLen = Hash.length(); // вычисляем длину строки
+ size_t length = Hash.copy(buf,msgLen); // копируем строку
+ send(Socket, buf, sizeof(buf), 0);// Отправка хэша-кода от SALT+PASSWORD
+ delete[] buf;
+ bytes_read = recv(sock, buf, sizeof(buf), 0);// Получене ответа об аутентификации
+ buf[bytes_read] = '\0';
+ string s3 = string(buf);
+ if (s3 == "ERR")
+ 	{
+    cerr << "Identification error" << endl;
+    close(Socket);
+    exit(1);
+    } 
+ }
+} 
+
+
+void connect::Send()//Отправка данных на сервер
+{ 
+void Operator::read(const char *original, vector<uint16_t>in);
+char buf [bufSize];
+int msgLen = in.size().length(); // вычисляем длину строки
+size_t length = str.copy(buf,msgLen); // копируем строку
+while (true)
+ {
+ rc = send(Socket, buf,msgLen,0);
+ if (rc == -1) {
+     cerr <<"Error send message\n";
+     close(Socket);
+     exit(1);
     }
-    cout<<"Data send\n";
-     
-    return true;
+ 	cout << "Data from " << origunal << " send" <<endl;
+ }
+ delete[] buf;
 }
 
-string connect::receive(int size=512,vector<uint16_t>out)
+void connect::Receive()  //Получение данных от сервера
 {
-    char buffer[size];
-    string reply;
-     
-    if( recv(sock , buffer , sizeof(buffer) , 0) < 0)
-    {
-        puts("recv failed");
+ char buf [bufSize];
+ int msgLen = str.length(); // вычисляем длину строки
+ size_t length = str.copy(buf,msgLen); // копируем строку
+ while (true) {
+ rc = recv(Socket, buf, bufSize,0);
+ if (rc == -1) {
+      cerr <<"Error receive answer\n";
+      close(Socket);
+      exit(1);
     }
-     
-    reply =vector<uint16_t>out;
-    return reply;
+ 	buf[rc]='\0';
+ 	void Operator::write(const char *Result, vector<uint16_t>out)
+ 	cout << "Data receive from  server write " << Result<<endl;
+ 	delete cAddr;
+	delete sAddr;
+ 	delete[] buf;
+ 	close(Socket);
 }
- 
