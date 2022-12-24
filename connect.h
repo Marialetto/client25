f13a0b3b87df4891d16cdde7b1e0b0ca071c000b
Marialetto <113329575+Marialetto@users.noreply.github.com>
2022-12-24 11:45:55 +0300

    Update connect.h

--- connect.h
+++ connect.h
@@ -1,28 +1,32 @@
 #pragma once
-#include<iostream>    //cout
-#include<stdio.h> //printf
-#include<string.h>    //strlen
-#include<string>  //string
-#include<sys/socket.h>    //socket
-#include<arpa/inet.h> //inet_addr
-#include<netdb.h> //hostent
- 
+#include <iostream>    //cout
+#include <string>  //string
+#include <sys/socket.h>    //socket
+#include <arpa/inet.h> //inet_addr
+#include <cstdlib>
+#include <unistd.h>
+#include <netinet/in.h>
 using namespace std;
+#define bufSize 512 
 
+#include <cryptopp/cryptlib.h>
+#include <cryptopp/hex.h>
+#include <cryptopp/files.h>
+#include <cryptopp/md5.h>
+using namespace CryptoPP;
+#define CRYPTOPP_ENABLE_NAMESPACE_WEAK 1
 class connect{
 private:
-vector<uint16_t>out;
-vector<uint16_t>out;
-   int sock;
-   string address;
-   unsigned short int port;
-   struct sockaddr_in server;
-   string username;
+	int sock;
+	string ip;
+	unsigned short int port;	
+	string username;
+	string auth;
+	string hash,Hash;
 public:
 	connect()=delete;
- 	connect()=delite;
-    connect(sock, port ,ip);
-    bool conn(string, int);
-    bool send_data(vector<uint16_t>out);
-    string receive(int);
+	connect(int Socket,unsigned short int port, string ip);//установка соединения
+   	void Auth(string auth);//авторизация
+    	void Send();//Отправка данных на сервер
+    	void Receive(); //Получение данных от сервера
 };
